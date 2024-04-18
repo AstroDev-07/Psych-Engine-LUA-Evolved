2929d3da1bcf5ad9220c034435e99e728c18eaa4
AstroDev-07 <160244951+AstroDev-07@users.noreply.github.com>
2024-04-18 17:23:15 +0100

    initial commit

---
+++ functions.lua
@@ -0,0 +1,10 @@
+
+-- LUA Evolved functions!!! (18/04/2024)
+
+printd('text') -- Prints the text ONLY to the debug console, pretty much an actual debug feature
+
+getSongInfo(curSong) -- returns small bits of info of the current song
+
+getRatingShit(curSong) -- returns current rating ui for song, Score, Misses, Rating and isFC?
+
+showErrorWindow('message','title') -- opens a error message with a custom title and body, default title: An error has occured
